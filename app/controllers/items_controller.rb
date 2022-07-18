b1628ffd02affb855b4e0d03b05d29aa202b6b11
shun1127 <watashun1127@gmail.com>
2022-07-19 06:54:16 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,21 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
   end
-  
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+   else
+     render :new
+   end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name,:content,:category_id,:state_id,:delivery_charge_id,:delivery_area_id,:delivery_day_id,:price).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
   #has_one :order_detail
   belongs_to :user
+  has_one_attached :image
 end
